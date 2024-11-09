8ec8930176874430667991b7e351c01645e18f3e
Egor Pankov <pankov.egor26032005@yandex.ru>
2024-11-09 10:40:06 +0300

    edit auth form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
+
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'signup', component: SignupComponent},
+    {path: '', component: HomeComponent}
+];
